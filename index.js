a7736b17ce197fa1850aff7ecc03eeff8a75793f
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-26 07:15:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const newDrivers = drivers.filter((driver) => {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+
+  if (newDrivers) {
+    return newDrivers;
+  } else {
+    return [];
+  }
+}
+
+function fuzzyMatch(drivers, startingLetters) {
+  const newDrivers = drivers.filter((driver) => {
+    return driver.startsWith(startingLetters);
+  });
+  return newDrivers;
+}
+
+function matchName(drivers, name) {
+  const newDrivers = drivers.filter((driver) => {
+    return driver.name === name;
+  });
+  return newDrivers;
+}
